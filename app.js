d62d71e51aef151500ace27318356dacdeb22ba1
Dani-Nicole <kuuskdani@gmail.com>
2023-01-08 19:43:30 +0200

    add project basic files.

---
+++ app.js
@@ -0,0 +1,20 @@
+const ui = new UI()
+
+const form = document.querySelector('#book-form')
+form.addEventListener('submit', addBook)
+
+function addBook(event){
+    // read user inputs
+    const title = ui.getInputData('#title')
+    const author = ui.getInputData('#author')
+    const isbn = ui.getInputData('#isbn')
+
+    const book = new Book(title, author, isbn)
+    ui.addBook(book)
+
+    ui.clearInputData('#title')
+    ui.clearInputData('#autor')
+    ui.clearInputData('#isbn')
+
+    event.preventDefault()
+}
